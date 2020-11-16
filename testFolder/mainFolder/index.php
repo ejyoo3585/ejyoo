f73efafe52ddd73c3146afde3d567dd217619bbd
ejyoo3585 <67091728+ejyoo3585@users.noreply.github.com>
2020-11-16 21:07:01 +0900

    first version

---
+++ testFolder/mainFolder/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Web 1 - WELCOME</title>
+        <meta charset="utf-8"/>
+    </head>
+
+    <body>
+        <h1><a href="./index.php">Web</a><h1>
+        <ol>
+            <li><a href="./1.php">HTML</a></li>
+            <li><a href="./mainFolder/2.php">CSS</a></li>
+            <li><a href="./mainFolder/3.php">JavaSctipt</a></li>
+        </ol>
+        <h2>WEB 이란 무엇인가? </h2>
+        <p><a href="http://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> 
+        and web applications. and web applications.Web browsers receive HTML documents from a web server or from local storage 
+        and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included 
+        cues for the appearance of the document.
+        <img src="img/coding.jpg" width="100%"/>
+        </p><p style="margin-top:45px">The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland.[2][3] The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.</p>
+    </body>
+</html>
